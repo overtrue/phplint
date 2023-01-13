4e5ad86ec854d3f274c5d6bb4829f119415b6c63
Laurent Laville <laurent.laville@gmail.com>
2023-01-13 11:41:46 +0000

    split code to specialized features: Logger component for cache debugging purpose

---
+++ src/Logger.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Overtrue\PHPLint;
+
+use Psr\Log\AbstractLogger;
+use Stringable;
+
+use function error_log;
+use function json_encode;
+
+/**
+ * Default PSR-3 logger (https://www.php-fig.org/psr/psr-3/).
+ *
+ * @author Laurent Laville
+ * @since Release 7.0.0
+ */
+final class Logger extends AbstractLogger
+{
+    public function log($level, Stringable|string $message, array $context = []): void
+    {
+        error_log(json_encode(['level' => $level, 'message' => $message, 'context' => $context]));
+    }
+}
