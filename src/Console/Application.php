8c800e14c33d7b83ba6283d0628da5c7e548c066
Laurent Laville <laurent.laville@gmail.com>
2023-01-13 12:29:24 +0000

    code-rewrites for v7 of Application class

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -4,15 +4,23 @@ declare(strict_types=1);
 
 namespace Overtrue\PHPLint\Console;
 
+use Overtrue\PHPLint\Command\LintCommand;
+use Overtrue\PHPLint\Event\EventDispatcher;
+use Overtrue\PHPLint\Extension\ProgressBar;
+use Overtrue\PHPLint\Extension\ProgressPrinter;
 use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
-class Application extends BaseApplication
+final class Application extends BaseApplication
 {
     public const NAME = 'phplint';
-
     public const VERSION = '7.0-dev';
+
+    private ?EventDispatcherInterface $dispatcher = null;
 
     public function __construct()
     {
@@ -28,8 +36,26 @@ class Application extends BaseApplication
         return $inputDefinition;
     }
 
+    public function getDispatcher(): ?EventDispatcherInterface
+    {
+        return $this->dispatcher;
+    }
+
     protected function getCommandName(InputInterface $input): string
     {
         return self::NAME;
+    }
+
+    protected function doRunCommand(Command $command, InputInterface $input, OutputInterface $output): int
+    {
+        if ($command instanceof LintCommand) {
+            $extensions = [
+                new ProgressPrinter(),
+                new ProgressBar(),
+            ];
+            $this->dispatcher = new EventDispatcher($extensions);
+            $this->setDispatcher($this->dispatcher);
+        }
+        return parent::doRunCommand($command, $input, $output);
     }
 }
