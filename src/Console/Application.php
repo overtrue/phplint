27ae22cd9c0eddec36480fb228294c8b3cdea6bf
Laurent Laville <laurent.laville@gmail.com>
2023-12-01 09:16:09 +0000

    bump new application version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -33,7 +33,7 @@ use function in_array;
 final class Application extends BaseApplication
 {
     public const NAME = 'phplint';
-    public const VERSION = '9.0.4';
+    public const VERSION = '9.0.5';
 
     public function __construct()
     {
