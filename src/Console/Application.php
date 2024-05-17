1e43e92e425c953f7c154460f0644517ea4f9317
Laurent Laville <laurent.laville@gmail.com>
2024-05-17 03:39:25 +0000

    prepare bugfix release 9.3.1

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -36,7 +36,7 @@ use function in_array;
 final class Application extends BaseApplication
 {
     public const NAME = 'phplint';
-    public const VERSION = '9.3.0';
+    public const VERSION = '9.3.1';
 
     public function __construct()
     {
