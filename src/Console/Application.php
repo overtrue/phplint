bb849f012fa44839f5ba4a079dda6d2ef29d6fce
Laurent Laville <laurent.laville@gmail.com>
2024-10-11 07:44:07 +0000

    prepare bugfix release 9.4.2

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -36,7 +36,7 @@ use const STDOUT;
 final class Application extends BaseApplication
 {
     public const NAME = 'phplint';
-    public const VERSION = '9.4.1';
+    public const VERSION = '9.4.2';
 
     public function __construct()
     {
