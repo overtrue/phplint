731f45c9ccc3d2867374e654795f40992defaa8a
Laurent Laville <laurent.laville@gmail.com>
2026-08-01 02:57:38 +0000

    upgrade the OutputManager extension (see #226)

--- src/Extension/OutputManager.php
+++ src/Extension/OutputManager.php
@@ -13,18 +13,24 @@ declare(strict_types=1);
 
 namespace Overtrue\PHPLint\Extension;
 
-use Overtrue\PHPLint\Command\LintCommand;
-use Overtrue\PHPLint\Configuration\ConsoleOptionsResolver;
 use Overtrue\PHPLint\Configuration\FileOptionsResolver;
 use Overtrue\PHPLint\Configuration\OptionDefinition;
 use Overtrue\PHPLint\Event\AfterCheckingEvent;
+use Overtrue\PHPLint\Event\BeforeCheckingEvent;
+use Overtrue\PHPLint\Event\Events;
+use Overtrue\PHPLint\Metadata\MetadataCollection;
 use Overtrue\PHPLint\Output\ChainOutput;
 use Overtrue\PHPLint\Output\FormatResolver;
+use Overtrue\PHPLint\Output\LinterOutput;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Symfony\Component\Console\ConsoleEvents;
 use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Event\ConsoleTerminateEvent;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Finder\Finder;
 
 /**
  * @author Laurent Laville
@@ -32,9 +38,14 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 final class OutputManager implements
     ExtensionInterface,
-    EventSubscriberInterface
+    EventSubscriberInterface,
+    LoggerAwareInterface
 {
+    use LoggerAwareTrait;
+
     private array $handlers = [];
+
+    private MetadataCollection $metadataCollection;
 
     public function getName(): string
     {
@@ -63,33 +74,71 @@ final class OutputManager implements
     public static function getSubscribedEvents(): array
     {
         return [
-            ConsoleEvents::COMMAND => 'initialize',
-            AfterCheckingEvent::class => 'finish',
+            ConsoleEvents::COMMAND => ['initialize', 100],
+            ConsoleEvents::TERMINATE => ['terminate', 100],
+            Events::BEFORE_CHECKING => ['beforeExecute', 100],
+            Events::AFTER_CHECKING => ['afterExecute', 100],
         ];
     }
 
     public function initialize(ConsoleCommandEvent $event): void
     {
         $command = $event->getCommand();
-        if (!$command->getCode() instanceof LintCommand) {
+        if ($command->getName() !== 'lint') {
             // this extension must be only available for lint command
             return;
         }
 
-        $input = $event->getInput();
+        $this->logger->debug(__METHOD__);
 
-        if (true === $input->hasParameterOption(['--no-configuration'], true)) {
-            $configResolver = new ConsoleOptionsResolver($input);
-        } else {
-            $configResolver = new FileOptionsResolver($input);
-        }
+        $this->metadataCollection = $command->getApplication()->getMetadata();
 
-        $this->handlers = (new FormatResolver())->resolve($configResolver, $event->getOutput());
+        $this->handlers = (new FormatResolver())->resolve(
+            new FileOptionsResolver($event->getInput()),
+            $event->getOutput(),
+        );
     }
 
-    public function finish(AfterCheckingEvent $event): void
+    public function terminate(ConsoleTerminateEvent $event): void
     {
+        $this->logger->debug(__METHOD__);
+
+        $metadataCollection = $this->metadataCollection ?? new MetadataCollection();
+
+        /** @var \Overtrue\PHPLint\Metadata\LinterOutput $results */
+        $finalResults = $metadataCollection->getMetadata(\Overtrue\PHPLint\Metadata\LinterOutput::class);
+
+        // Only to keep API Backward Compatible with version 9.7.x
+        // Will be removed in next API version
+        if (null === $finalResults) {
+            // no result available
+            $results = new LinterOutput([], new Finder());
+        } else {
+            $results = new LinterOutput(
+                [
+                    'errors' => $finalResults->getErrors(),
+                    'warnings' => $finalResults->getWarnings(),
+                    'hits' => $finalResults->getHits(),
+                    'misses' => $finalResults->getMisses(),
+                ],
+                $finalResults->getFinder()
+            );
+        }
+
         $outputHandler = new ChainOutput($this->handlers);
-        $outputHandler->format($event->getArgument($event::SCAN_RESULTS));
+        $outputHandler->format($results, $metadataCollection);
+    }
+
+    public function beforeExecute(BeforeCheckingEvent $event): void
+    {
+        $this->logger->debug(
+            __METHOD__ . ' ; file count queued to process: {' . BeforeCheckingEvent::FILE_COUNT . '}',
+            [BeforeCheckingEvent::FILE_COUNT => $event->getArgument(BeforeCheckingEvent::FILE_COUNT)]
+        );
+    }
+
+    public function afterExecute(AfterCheckingEvent $event): void
+    {
+        $this->logger->debug(__METHOD__);
     }
 }
