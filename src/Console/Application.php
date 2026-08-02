c04f1921424696c0f2fe65c9992405b615dbdd2a
Laurent Laville <laurent.laville@gmail.com>
2026-08-02 03:53:35 +0000

    upgrade the Console Application to use latest changes (SF 8.1+ compatibility, internal InvokableCommand to use with ArgumentResolver that may be used with SF 7.4, Metadata collection, load extensions on demand in goal to #217)

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -13,11 +13,15 @@ declare(strict_types=1);
 
 namespace Overtrue\PHPLint\Console;
 
+use Overtrue\PHPLint\Command\InvokableCommand;
 use Overtrue\PHPLint\Configuration\OptionDefinition;
+use Overtrue\PHPLint\Configuration\Resolver\ArgumentResolverInterface;
 use Overtrue\PHPLint\Configuration\Resolver\PluginValueResolver;
 use Overtrue\PHPLint\Extension\ExtensionEnum;
 use Overtrue\PHPLint\Extension\ExtensionInterface;
+use Overtrue\PHPLint\Metadata\ApplicationVersion;
 use Overtrue\PHPLint\Metadata\Metadata;
+use Overtrue\PHPLint\Metadata\MetadataCollection;
 use Overtrue\PHPLint\Output\ConsoleOutput;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
@@ -25,24 +29,26 @@ use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 use ReflectionException;
 use ReflectionFunction;
-use stdClass;
 use Symfony\Component\Console\Application as BaseApplication;
 use Symfony\Component\Console\Attribute\Reflection\ReflectionMember;
-use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use Symfony\Component\Console\Command\HelpCommand;
 use Symfony\Component\Console\Command\ListCommand;
 use Symfony\Component\Console\Exception\CommandNotFoundException;
 use Symfony\Component\Console\Exception\ExceptionInterface;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Helper\FormatterHelper;
 use Symfony\Component\Console\Helper\HelperSet;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Stopwatch\Stopwatch;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
-use function sprintf;
-use function substr;
+use function array_filter;
+use function iterator_to_array;
+use function json_encode;
 
 /**
  * @author Overtrue
@@ -52,39 +58,62 @@ final class Application extends BaseApplication implements ApplicationInterface,
 {
     use LoggerAwareTrait;
 
-    private stdClass $metaApplicationVersion;
+    private ArgumentResolverInterface $argumentResolver;
 
     private EventDispatcherInterface $dispatcher;
+
+    private ?Stopwatch $stopwatch = null;
+
+    private MetadataCollection $metadataCollection;
 
     public function __construct()
     {
         parent::__construct();
+
         $this->dispatcher = new EventDispatcher();
         // mandatory because $dispatcher instance of BaseApplication is private
+        // and native getDispatcher() method is only available with release 8.1.0 of Symfony/Console
         $this->setDispatcher($this->dispatcher);
 
-        $this->metaApplicationVersion = Metadata::applicationVersion()->describe();
-        $this->setVersion($this->metaApplicationVersion->value);
+        $applicationVersion = Metadata::applicationVersion();
+        $this->setVersion($applicationVersion->getVersion());
+        $this->metadataCollection = new MetadataCollection($applicationVersion);
+    }
+
+    /**
+     * Equivalent to setArgumentResolver() on Symfony 8.1 Console Component with compatible contract
+     */
+    public function setArgResolver(ArgumentResolverInterface $argumentResolver): void
+    {
+        $this->argumentResolver = $argumentResolver;
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function addCommand(callable|SymfonyCommand $command): ?SymfonyCommand
+    {
+        if (!$command instanceof SymfonyCommand) {
+            $code = $command;
+            $command = new SymfonyCommand();
+            $invokableCommand = new InvokableCommand($command, $code, $this->argumentResolver);
+            $command->setName($invokableCommand->getName());
+            $command->setDescription($invokableCommand->getDescription());
+            $command->setCode($invokableCommand);
+
+            $definition = $command->getDefinition();
+            // mandatory, otherwise arguments and options of the $command won't be added
+            $invokableCommand->configure($definition);
+        }
+        parent::addCommand($command);
+        return $command;
     }
 
     public function getLongVersion(): string
     {
-        $appName = $this->metaApplicationVersion->description;
-        $appVersion = json_decode($this->metaApplicationVersion->value, true);
-
-        $version = ('UNKNOWN' !== $appVersion['pretty_version'])
-            ? $appVersion['pretty_version']
-            : $appVersion['semantic_version']
-        ;
-        $shortRef = substr($appVersion['reference'], 0, 7);
-
-        return ('UNKNOWN' === $appName)
-            ? 'PHPLint'
-            : sprintf(
-                '%s <info>%s</info> <comment>(%s)</comment> by overtrue and contributors.',
-                $appName, $version, $shortRef
-            )
-        ;
+        /** @var ApplicationVersion $applicationVersion */
+        $applicationVersion = $this->metadataCollection->getMetadata(ApplicationVersion::class);
+        return $applicationVersion->getLongVersion();
     }
 
     public function getLogger(): LoggerInterface
@@ -95,22 +124,34 @@ final class Application extends BaseApplication implements ApplicationInterface,
         return $this->logger;
     }
 
+    public function getProfiler(): ?Stopwatch
+    {
+        return $this->stopwatch;
+    }
+
     /**
      * Officially introduced with version 8.1 of Symfony Console Component
      * @link https://github.com/symfony/console/blob/2b468472ec5d0e4acbe00f97e62f6cd552509894/Application.php#L115-L118
      */
-    public function getEventDispatcher(): ?EventDispatcherInterface
+    public function getDispatcher(): ?EventDispatcherInterface
     {
         return $this->dispatcher;
+    }
+
+    public function getMetadata(): MetadataCollection
+    {
+        return $this->metadataCollection;
     }
 
     public function run(?InputInterface $input = null, ?OutputInterface $output = null): int
     {
         $output ??= new ConsoleOutput();
 
-        // @fixme Will be removed later when Symfony/Runtime component will be implemented
-        if (null === $this->logger) {
-            $this->setLogger(new \Symfony\Component\Console\Logger\ConsoleLogger($output));
+        if (!$output->getFormatter()->hasStyle('profile')) {
+            $output->getFormatter()->setStyle(
+                'profile',
+                new OutputFormatterStyle('black', 'gray')
+            );
         }
 
         return parent::run($input, $output);
@@ -120,7 +161,7 @@ final class Application extends BaseApplication implements ApplicationInterface,
     {
         if (true === $input->hasParameterOption(['--version', '-V'], true)) {
             $output->writeln($this->getLongVersion());
-            return Command::SUCCESS;
+            return SymfonyCommand::SUCCESS;
         }
 
         try {
@@ -131,10 +172,10 @@ final class Application extends BaseApplication implements ApplicationInterface,
         }
 
         $name = $this->getCommandName($input);
-        if ($name) {
+        if ('lint' === $name) {
             try {
                 $command = $this->find($name);
-                $this->loadPlugins($command, $input);
+                $this->loadPlugins($command, $input, $output);
             } catch (CommandNotFoundException) {
                 // fallback to Symfony Base Application (parent) that will handle this error
             } catch (ReflectionException) {
@@ -182,11 +223,13 @@ final class Application extends BaseApplication implements ApplicationInterface,
     /**
      * @throws ReflectionException
      */
-    private function resolvePlugins(Command $command, InputInterface $input): iterable
+    private function resolvePlugins(SymfonyCommand $command, InputInterface $input): iterable
     {
-        $code = $command->getCode();
+        $invokableCommand = $command->getCode();
 
-        if ($code === null) {
+        $code = $invokableCommand?->getCode() ?? null;
+
+        if (null === $code) {
             return [];
         }
 
@@ -207,15 +250,34 @@ final class Application extends BaseApplication implements ApplicationInterface,
     /**
      * @throws ReflectionException
      */
-    private function loadPlugins(Command $command, InputInterface $input): void
+    private function loadPlugins(SymfonyCommand $command, InputInterface $input, OutputInterface $output): void
     {
         $dispatcher = $this->getDispatcher();
+        $logger = $this->getLogger();
 
-        $extensions = $this->resolvePlugins($command, $input);
+        $extensions = array_filter(
+            iterator_to_array($this->resolvePlugins($command, $input))
+        );
+
+        $logger->notice(
+            'The "{command}" command load the following extensions: {extensions}}',
+            [
+                'command' => $command->getName(),
+                'extensions' => json_encode($extensions),
+            ]
+        );
 
         // loads all valid extension on fly, invalid ones will be reported by the Console ArgumentResolver component
         foreach ($extensions as $extensionName) {
             $extension = ExtensionEnum::factory($extensionName);
+
+            if ($extensionName === ExtensionEnum::PROFILE_MANAGER) {
+                $this->stopwatch = new Stopwatch(true);
+            }
+
+            if ($extension instanceof LoggerAwareInterface) {
+                $extension->setLogger($logger);
+            }
 
             if ($extension instanceof EventSubscriberInterface) {
                 $dispatcher->addSubscriber($extension);
