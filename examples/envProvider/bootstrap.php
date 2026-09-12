621d57411e9ce275dc511a4365c4a7c61803c1c8
Laurent Laville <laurent.laville@gmail.com>
2026-09-12 05:17:54 +0000

    add header template

--- examples/envProvider/bootstrap.php
+++ examples/envProvider/bootstrap.php
@@ -1,3 +1,12 @@
 <?php
 
+/*
+ * This file is part of the overtrue/phplint package
+ *
+ * (c) overtrue
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 require __DIR__ . '/MyEnvProvider.php';
