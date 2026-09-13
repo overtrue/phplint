5ec3f1364a950ec01b0146cbb7063cb32dc45a5a
Laurent Laville <laurent.laville@gmail.com>
2026-09-13 04:05:11 +0000

    FileOptionsResolver accept now all three formats YAML, JSON, PHP for external configuration file

--- src/Configuration/FileOptionsResolver.php
+++ src/Configuration/FileOptionsResolver.php
@@ -13,6 +13,8 @@ declare(strict_types=1);
 
 namespace Overtrue\PHPLint\Configuration;
 
+use JsonException;
+use RuntimeException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
 use Symfony\Component\Yaml\Exception\ParseException;
@@ -20,8 +22,16 @@ use Symfony\Component\Yaml\Yaml;
 
 use function array_filter;
 use function array_replace;
+use function file_get_contents;
 use function is_array;
+use function is_file;
+use function is_readable;
+use function json_decode;
+use function pathinfo;
 use function sprintf;
+
+use const JSON_THROW_ON_ERROR;
+use const PATHINFO_EXTENSION;
 
 /**
  * @author Laurent Laville
@@ -48,8 +58,8 @@ final class FileOptionsResolver extends AbstractOptionsResolver
         }
 
         if (!empty($configFile)) {
-            $yamlConf = $this->parseYamlConfiguration($configFile);
-            $configuration = array_replace(array_filter($yamlConf), array_filter($configuration));
+            $fileConf = $this->parseFile($configFile);
+            $configuration = array_replace(array_filter($fileConf), array_filter($configuration));
             $configuration[OptionDefinition::CONFIGURATION] = $configFile;
         }
         parent::__construct($input, $configuration);
@@ -58,6 +68,21 @@ final class FileOptionsResolver extends AbstractOptionsResolver
     public function factory(): Options
     {
         return new OptionsFactory($this->defaults);
+    }
+
+
+    private function parseFile(string $filename): array
+    {
+        $configuration = match (pathinfo($filename, PATHINFO_EXTENSION)) {
+            'yml', 'yaml' => $this->parseYamlConfiguration($filename),
+            'json' => $this->parseJsonConfiguration($filename),
+            'php' => $this->parsePhpConfiguration($filename),
+            default => throw new InvalidOptionsException(sprintf('File format "%s" is not accepted.', $filename))
+        };
+
+        // Checks that config file contents adhere to configuration syntax
+        $factory = new OptionsFactory([]);
+        return $factory->resolve($configuration);
     }
 
     private function parseYamlConfiguration(string $filename): array
@@ -78,8 +103,39 @@ final class FileOptionsResolver extends AbstractOptionsResolver
             throw new InvalidOptionsException(sprintf('Invalid content type in "%s".', $filename));
         }
 
-        // Checks that YAML config file contents adhere to configuration syntax
-        $factory = new OptionsFactory([]);
-        return $factory->resolve($configuration);
+        return $configuration;
+    }
+
+    private function parseJsonConfiguration(string $filename): array
+    {
+        if (!is_file($filename)) {
+            throw new RuntimeException(sprintf('File "%s" does not exist.', $filename));
+        }
+
+        if (!is_readable($filename)) {
+            throw new RuntimeException(sprintf('File "%s" cannot be read.', $filename));
+        }
+
+        try {
+            $configuration = json_decode(file_get_contents($filename), true, flags: JSON_THROW_ON_ERROR);
+        } catch (JsonException $e) {
+            throw new InvalidOptionsException(
+                sprintf('Invalid content type in "%s".', $filename),
+                $e->getCode(),
+            );
+        }
+
+        return $configuration;
+    }
+
+    private function parsePhpConfiguration(string $filename): array
+    {
+        $configuration = require $filename;
+
+        if (!is_array($configuration)) {
+            throw new InvalidOptionsException(sprintf('Invalid content type in "%s".', $filename));
+        }
+
+        return $configuration;
     }
 }
